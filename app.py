a3fc368b4d540bdac4a494a45a369d7048da9700
root <root@python.us-central1-f.c.nifty-inkwell-462806-n1.internal>
2025-07-04 09:29:14 +0000

    add files

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "🚀 Hello from your Flask app on GCP!"
+
+@app.route('/about')
+def about():
+    return "This is a simple web app for practicing Git & GCP!"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
